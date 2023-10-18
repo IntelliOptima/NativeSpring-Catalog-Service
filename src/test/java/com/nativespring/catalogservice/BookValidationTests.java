635aec6a2971a80fee02c7543be66803fcb5b690
Alex793x <alex793x@stud.kea.dk>
2023-10-18 02:00:55 +0200

    added tests cases for controller, and service

---
+++ src/test/java/com/nativespring/catalogservice/BookValidationTests.java
@@ -0,0 +1,42 @@
+package com.nativespring.catalogservice;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.nativespring.catalogservice.domain.Book;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+public class BookValidationTests {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds() {
+        var book = new Book("1234567890", "Title", "Author", 9.99);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectTHenValidationFails() {
+        var book = new Book("123456789", "Title", "Author", 9.99);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+            .isEqualTo("The book ISBN format must be valid.");
+    }
+}
